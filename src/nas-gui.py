95cdc091dbe03acfa0abaa6588cebafa2cfde5f9
seigneurfuo <seigneurfuo@protonmail.com>
2021-01-02 02:49:40 +0100

    - Ajout d'un protocol de connection en sshds afin de permetre de la lecture seule sur certains répertoires. Pas de MDP    demandé: Il faut copier ces clefs SSH avec la commande: <i>ssh-copy-id</i>  - Ajout d'un message d'erreur si le fichier de configuration n'existe pas

--- src/nas-gui.py
+++ src/nas-gui.py
@@ -3,17 +3,24 @@
 import sys
 import os
 import configparser
+import shutil
 
 from pathlib import Path
 from webbrowser import open_new_tab
 
-# PyQt5
-from PyQt5.QtWidgets import QApplication, QMessageBox, QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtWidgets import QApplication, QMessageBox, QSystemTrayIcon, QMenu
 from PyQt5.QtGui import QIcon
 
 changelog_message = """
+<b>2020-01-01</b>
+ - Bonne année 2021!
+ - Ajout d'un protocol de connection en sshds afin de permetre de la lecture seule sur certains répertoires. Pas de MDP
+   demandé: Il faut copier ces clefs SSH avec la commande: <i>ssh-copy-id</i>
+ - Ajout d'un message d'erreur si le fichier de configuration n'existe pas
+ 
+
 <b>2020-05-31</b>
- - Possibilité de configurer le mode de montage (SMB ou NFS dans le fichier<br>de configuration
+ - Possibilité de configurer le mode de montage (SMB ou NFS) dans le fichier<br>de configuration
 
 <b>2020-05-01</b>
  - Ajout d'un nouveau système dynamique pour la création des entrées dans le menu
@@ -21,7 +28,7 @@ changelog_message = """
  - Nettoyage du code
 
 <b>2020-04-30</b>
- - Déplacement de quelques informations de configuration dans un fichier<br>de configuration: ~/.config/nas-gui.ini
+ - Déplacement de quelques informations de configuration dans un fichier<br>de configuration: <b><i>~/.config/nas-gui.conf</i></b>
  - Désactivation de la demande de montage quand pamac est ouvert
 
 <b>2020-04-09</b>
@@ -32,7 +39,7 @@ changelog_message = """
  - Correction de la fermeture de l'application
 
 <b>2020-03-15</b>
- - Ajout d'un .desktop pour lance rl'application à chaque démarrage
+ - Ajout d'un .desktop dans <b><i>/etc/xdg/autostart/</i></b> pour lancer l'application à chaque démarrage
 
 <b>2019-07-20</b>
  - Ajout d'une entrée "changements"
@@ -49,7 +56,7 @@ class TrayIcon(QSystemTrayIcon):
         self.read_config_file()
 
         # Chemin de base des partages sur le NAS
-        self.nas_smb_base_folder = ""
+        self.nas_sshfs_base_folder = "{nas_ip}:".format(nas_ip=self.config["Settings"]["ip"])
         self.nas_nfs_base_folder = "{nas_ip}:/volume1".format(nas_ip=self.config["Settings"]["ip"])
 
         # Point de montage par défaut
@@ -68,7 +75,7 @@ class TrayIcon(QSystemTrayIcon):
 
             {"name": "Changements",
                  "icon": "",
-                 "action": self.show_changements,
+                 "action": self.show_changelog,
                  "separator": None},
 
             {"name": "Démonter TOUS les partages",
@@ -87,7 +94,7 @@ class TrayIcon(QSystemTrayIcon):
 
     def read_config_file(self):
         self.config = configparser.ConfigParser()
-        self.config_file_path = os.path.join(Path.home(),".cdonfig", "nas-gui.conf")
+        self.config_file_path = os.path.join(Path.home(),".config", "nas-gui.conf")
 
         if not os.path.isfile(self.config_file_path):
             msg = "Fichier de configuration inexistant ! <br> Veuillez renseigner la configuration dans le fichier: <br><b>{}</b>".format(self.config_file_path)
@@ -116,7 +123,8 @@ class TrayIcon(QSystemTrayIcon):
             label = "{name} ({protocol})".format(name=share_name, protocol=protocol.upper())
 
             menu_action = self.menu.addAction(icon, label)
-            menu_action.triggered.connect(lambda lamdba, share_name=share_name: self.mount_share(share_name))
+            menu_action.setCheckable(False)
+            menu_action.triggered.connect(lambda lamdba, share_name=share_name, menu_action=menu_action: self.mount_share(share_name, menu_action))
 
         self.menu.addSeparator()
 
@@ -137,18 +145,20 @@ class TrayIcon(QSystemTrayIcon):
         self.setIcon(QIcon.fromTheme(self.config["Settings"]["tray_icon"]))
         self.setVisible(True)
 
+    def on_systray_activated(self, i_reason):
+        self.menu.show()
 
-    def show_changements(self):
+
+    def show_changelog(self):
         """
         Affiche la fenêtre des changements
 
         :return: None
         """
 
-        dialog = QMessageBox()
-        dialog.setWindowTitle("Changements")
-        dialog.setText("<p>" + changelog_message.replace("\n", "<br>") + "</p>")
-        dialog.exec()
+        msg = "<p>" + changelog_message.replace("\n", "<br>") + "</p>"
+        dialog = QMessageBox(QMessageBox.Information, "Liste des changements", msg)
+        dialog.exec_()
 
 
     def open_DSM(self):
@@ -172,7 +182,7 @@ class TrayIcon(QSystemTrayIcon):
         os.system(command)
 
 
-    def mount_share(self, share_name):
+    def mount_share(self, share_name, menu_action):
         """
         Fonction pour monter un partage grace à son nom
 
@@ -189,24 +199,30 @@ class TrayIcon(QSystemTrayIcon):
             local_path = "/var/cache/pacman/pkg"
             self.mount_nfs(remote_path, local_path)
 
-
         local_path = "{}/{}".format(self.mountpoint, share_name)
+        remote_path = "{}/{}".format(self.nas_sshfs_base_folder, share_name)
+
         # Création automatique du dossier de partage
         if not os.path.exists(local_path):
             os.makedirs(local_path)
 
-        # SMB
-        if  self.config[share_name]["protocol"] == "smb":
-            remote_path = "{}/{}".format(self.nas_smb_base_folder, share_name)
-            self.mount_smb(remote_path, local_path)
+        # Si déja monté
+        if os.path.ismount(local_path):
+            msg = "Le partage {} est déja monté".format(local_path)
+            self.showMessage("Partage déja monté", msg, 1000)
 
-        # NFS
+        # Si pas monté
         else:
-            remote_path = "{}/{}".format(self.nas_nfs_base_folder, share_name)
-            self.mount_nfs(remote_path, local_path)
+            # sshfs
+            if self.config[share_name]["protocol"] == "sshfs":
+                self.mount_sshfs(remote_path, local_path, menu_action)
+
+            # NFS
+            elif self.config[share_name]["protocol"] == "nfs":
+                self.mount_nfs(remote_path, local_path, menu_action)
 
 
-    def mount_nfs(self, remote_path, local_path):
+    def mount_nfs(self, remote_path, local_path, menu_action=None):
         """
         Fonction qui permet de monter un chemin distant
 
@@ -219,14 +235,28 @@ class TrayIcon(QSystemTrayIcon):
             self.showMessage("Partage déja monté", "Le partage selectionné est déja monté", 1000)
 
         else:
-            command = "pkexec mount {} {}".format(remote_path, local_path)
+            command = "pkexec mount {remote_path} {local_path}".format(remote_path=remote_path, local_path=local_path)
             #command = "mount -t cifs {} {} -o username={},password={}".format(remotePath, localPath, user, password) #,uid=1000,gid=1000
             os.system(command)
             self.showMessage("Commande", command, 1000)
 
-    def mount_smb(self, remote_path, local_path):
-        pass
+        # On coche l'action dans le menu pour indique le dossier est bien monté
+        if menu_action: # Pour contourner Package qui monte sans action
+            menu_action.setCheckable(True)
+            menu_action.setChecked(True)
 
+    def mount_sshfs(self, remote_path, local_path, menu_action):
+        if not shutil.which("sshfs"):
+            msg = "Le montage <b>{local_path}</b> est configuré pour etre monté avec sshfs. <br>Cependant l'executable sshfs n'est pas installé.".format(local_path=local_path)
+            qmessagebox = QMessageBox(QMessageBox.Critical, "Executable inexistant", msg)
+            qmessagebox.show()
+            qmessagebox.exec_()
+            exit(0)
+
+        else :
+            command = "sshfs {username}@{remote_path} {local_path} -o ro".format(username=self.config["Settings"]["sshfs_user"], remote_path=remote_path, local_path=local_path)
+            os.system(command)
+            self.showMessage("Commande", command, 1000)
 
     def close(self):
         self.setVisible(False)
